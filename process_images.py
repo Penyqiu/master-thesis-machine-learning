88a8bd3f8b3ee7913fbe88cb11c00add082fd104
penyqiu <kpenczynski@gmail.com>
2023-12-12 20:00:40 +0100

    fix brah

--- process_images.py
+++ process_images.py
@@ -1,25 +1,23 @@
 import cv2
 import os
 
-def resize_image_to_80x80(image_path, output_path):
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    resized_img = cv2.resize(img, (80, 80))
-    cv2.imwrite(output_path, resized_img)
+def process_images_from_folder(folder_name, output_folder, size=(80, 80)):
+    for filename in os.listdir(folder_name):
+        if filename.endswith(".png"):
+            img_path = os.path.join(folder_name, filename)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-def process_images_from_folder(input_folder, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    for filename in os.listdir(input_folder):
-        if filename.endswith(".jpg"):
-            input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename)
-            resize_image_to_80x80(input_path, output_path)
+            resized_img = cv2.resize(img, size)
 
-input_folder_open = 'train/Open_Eyes'
-input_folder_closed = 'train/Closed_Eyes'
-output_folder_open = 'train/output/open_eyes'
-output_folder_closed = 'train/output/closed_eyes'
+            cv2.imwrite(os.path.join(output_folder, filename), resized_img)
+
+input_folder_open = 'C:\\Users\\Razer\\Desktop\\magisterka\\master-thesis-machine-learning\\train\\Open_Eyes'
+input_folder_closed = 'C:\\Users\\Razer\\Desktop\\magisterka\\master-thesis-machine-learning\\train\\Closed_Eyes'
+output_folder_open = 'C:\\Users\\Razer\\Desktop\\magisterka\\master-thesis-machine-learning\\train\\output\\open_eyes'
+output_folder_closed = 'C:\\Users\\Razer\\Desktop\\magisterka\\master-thesis-machine-learning\\train\\output\\closed_eyes'
+
+os.makedirs(output_folder_open, exist_ok=True)
+os.makedirs(output_folder_closed, exist_ok=True)
 
 process_images_from_folder(input_folder_open, output_folder_open)
 process_images_from_folder(input_folder_closed, output_folder_closed)

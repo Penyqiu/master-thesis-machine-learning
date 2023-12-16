16289f56558d6c0c73d04075e4afdddd40557c9c
penyqiu <kpenczynski@gmail.com>
2023-12-16 16:56:48 +0100

    Oczka bez modelu

--- cascade_classifier.py
+++ cascade_classifier.py
@@ -1,7 +1,5 @@
 import cv2
 
-model
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
@@ -13,40 +11,12 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
 
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in eyes:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-
-        for (ex, ey, ew, eh) in eyes:
-            eye_roi_gray = roi_gray[ey:ey+eh, ex:ex+ew]
-            eye_roi_color = roi_color[ey:ey+eh, ex:ex+ew]
-
-            eye_roi_prepared = cv2.resize(eye_roi_gray, (width, height))
-            eye_roi_prepared = eye_roi_prepared / 255.0
-            eye_roi_prepared = np.expand_dims(eye_roi_prepared, axis=0)
-
-            prediction = model.predict(eye_roi_prepared)
-            predicted_class = np.argmax(prediction)
-            if predicted_class == 0:
-                eye_status = "Open"
-            elif predicted_class == 1:
-                eye_status = "Closed"
-            else:
-                eye_status = "Unknown"
-
-            cv2.putText(roi_color, eye_status, (ex, ey - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 1)
-
-    cv2.imshow('Frame', frame)
+    cv2.imshow('Wykrywanie oczu', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()

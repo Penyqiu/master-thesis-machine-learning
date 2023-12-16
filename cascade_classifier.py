cf5b45f9dac8b6076133ca6ab565db621aa20ed7
penyqiu <kpenczynski@gmail.com>
2023-12-16 17:22:04 +0100

    tuning for not detecting camera

--- cascade_classifier.py
+++ cascade_classifier.py
@@ -11,7 +11,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
+    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in eyes:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
